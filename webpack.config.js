df4d23283eda102a50f183c357e6d42d1715e442
timeTravelCYN <554793916@qq.com>
2017-08-01 23:15:01 +0800

    react脚手架初始化

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const OpenBrowserPlugin = require('open-browser-webpack-plugin')
+
+
+module.exports = {
+  
+}
